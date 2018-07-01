169f174fd03675848aeb4fa6d29ed73405975c5d
Douglas Lima <douglasproglima@gmail.com>
2018-07-01 19:49:44 +0000

    Add o projeto ao git

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+# Criando nossos Users ---
+# OBS: Depois que adicionarmos o devise precisamos incluir o email e senha dos users
+User.create name: 'Douglas Lima', status: :payd, kind: :user, email: 'douglasproglima@gmail.com', password: 123456
